5a0f2a27dfc58405a035d115cc38c5f9dd6cb135
Umar Ali <engr.umarali@gmail.com>
2020-05-12 13:18:08 -0700

    this commit has to revert just trying a web app

---
+++ precache-manifest.3ed3e86b770ca87f72e724dd54d4bfd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f25814534cf022419ee819cda5282f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68b4ef1c830855c46041",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b4ef1c830855c46041",
+    "url": "/static/js/main.b7fb517e.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
