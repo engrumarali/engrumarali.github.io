7f41289f71cb9f2de59c7d7c87526399cab7253a
Umar Ali <engr.umarali@gmail.com>
2020-07-03 05:47:13 -0700

    Try to update service worker cache

--- demo.js
+++ demo.js
@@ -1,9 +1,60 @@
 function registerSW() {
   console.log('Registering ServiceWorker.');
-  navigator.serviceWorker.register('service-worker.js');
+  navigator.serviceWorker
+  .register('service-worker.js')
+  .then(function(registration) {
+    // registration worked
+    console.log('Registration succeeded.');
+    const installingWorker = registration.installing;
+        if (installingWorker == null) {
+          return;
+        }
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              // At this point, the updated precached content has been fetched,
+              // but the previous service worker will still serve the older
+              // content until all client tabs are closed.
+              console.log(
+                'New content is available and will be used when all ' +
+                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+              );
+
+              // // Execute callback
+              // if (config && config.onUpdate) {
+              //   config.onUpdate(registration);
+              // }
+            } else {
+              // At this point, everything has been precached.
+              // It's the perfect time to display a
+              // "Content is cached for offline use." message.
+              console.log('Content is cached for offline use.');
+
+              // // Execute callback
+              // if (config && config.onSuccess) {
+              //   config.onSuccess(registration);
+              // }
+            }
+          }
+        };
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
 }
 
 window.navigator.serviceWorker.onmessage = function (event) {
   if (event.data.command === 'SET_OFFLINE_READY')
     precorWorkout.setDefaultURL();
 };
+
+navigator.serviceWorker.register('/sw-test/sw.js', {scope: 'sw-test'}).then(function(registration) {
+  // registration worked
+  console.log('Registration succeeded.');
+  button.onclick = function() {
+    registration.update();
+  }
+}).catch(function(error) {
+  // registration failed
+  console.log('Registration failed with ' + error);
+});
