cea04ef65af092897836b7a33481499c47981bd8
Umar Ali <engr.umarali@gmail.com>
2020-07-03 23:04:21 -0700

    using build from create-react-app

--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,39 @@
-const PRECACHE = 'precache-v1-4';
-const PRECACHE_URLS = ['index.html', './', 'demo.js'];
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-const expectedCaches = ['precache-v1-4'];
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-self.addEventListener('install', event => {
-  console.log('precache-v1-4 installing…');
+importScripts(
+  "/precache-manifest.81ebd9a91ade5f8aafb67a91ecb37478.js"
+);
 
-  // cache a horse SVG into a new cache, precache-v1-3
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  // delete any caches that aren't in expectedCaches
-  // which will get rid of static-v1
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (!expectedCaches.includes(key)) {
-          return caches.delete(key);
-        }
-      })
-    )).then(() => {
-      console.log('precache-v1-4 now ready to handle fetches!');
-    })
-  );
-  self.clients.claim();
-  self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
-      console.log("💎 ------- send SET_OFFLINE_READY " + client);
-      client.postMessage({
-        command: 'SET_OFFLINE_READY',
-      });
-    });
-  });
-});
-
-self.addEventListener('fetch', (event) => {
-  console.log('🚩 Fetch Event: ' + event.request.url);
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          console.log('💠 Fetch Cache');
-          return cachedResponse;
-        }
-        return fetch(event.request).then((response) => {
-          console.log('💡 Fetch Internet');
-          return response;
-        });
-      })
-    );
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
   }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
